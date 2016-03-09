16bd6b455c06cab7a92453abd7be0cc2171e6a39
Park Chungwoo <cww.park@samsung.com>
2016-03-09 14:35:36 +0900

    thermal: of-thermal: Add governor parsing from device-tree
    Change-Id: Ia63bc75ab4595bcf969402c8ea737cb8289b8292
    Signed-off-by: Park Chungwoo <cww.park@samsung.com>
    Signed-off-by: Hyeonseong Gil <hs.gil@samsung.com>

--- drivers/thermal/of-thermal.c
+++ drivers/thermal/of-thermal.c
@@ -973,6 +973,7 @@ int __init of_parse_thermal_zones(void)
 	for_each_available_child_of_node(np, child) {
 		struct thermal_zone_device *zone;
 		struct thermal_zone_params *tzp;
+		const char *governor_name;
 		int i, mask = 0;
 		u32 prop;
 
@@ -1006,6 +1007,9 @@ int __init of_parse_thermal_zones(void)
 		/* these two are left for temperature drivers to use */
 		tzp->slope = tz->slope;
 		tzp->offset = tz->offset;
+
+		if (!of_property_read_string(child, "governor", &governor_name))
+			strncpy(tzp->governor_name, governor_name, THERMAL_NAME_LENGTH);
 
 		zone = thermal_zone_device_register(child->name, tz->ntrips,
 						    mask, tz,
