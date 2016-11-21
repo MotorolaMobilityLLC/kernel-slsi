f6928be96e6ed20be9678cc6dfd6519179c5f721
Youngmin Nam <youngmin.nam@samsung.com>
2016-11-21 15:42:11 +0900

    [COMMON] i2c: s3c2410: prepare I2C clock once on probe
    Change-Id: I71ac919cc793edbe2dab2587c225d7a3e61fee22
    Signed-off-by: Youngmin Nam <youngmin.nam@samsung.com>

--- drivers/i2c/busses/i2c-s3c2410.c
+++ drivers/i2c/busses/i2c-s3c2410.c
@@ -1213,6 +1213,11 @@ static int s3c24xx_i2c_probe(struct platform_device *pdev)
 
 	dev_dbg(&pdev->dev, "clock source %p\n", i2c->clk);
 
+	ret = clk_prepare(i2c->clk);
+	if (ret) {
+		dev_err(&pdev->dev, "I2C clock prepare failed\n");
+		return ret;
+	}
 
 	/* map the registers */
 
