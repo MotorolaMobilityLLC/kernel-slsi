85178ce0cc3982124e83e4e39db9d046dd648478
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2016-12-19 13:13:51 +0000

    ASoC: madera: Fix virtual input mux
    The virtual input muxes should have been attached back to the
    DSP widget when the preloader was turned into a supply widget,
    as you can't have a DAPM route going from a non-supply widget to
    a supply widget. ie. Supply widgets can only be the source of a
    path with non-supply widgets. This patch makes this change.
    
    Change-Id: I4e1e55857416397c961a7d2c80b016eff7be7404
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cs47l35.c
+++ sound/soc/codecs/cs47l35.c
@@ -1487,9 +1487,9 @@ static const struct snd_soc_dapm_route cs47l35_dapm_routes[] = {
 	MADERA_DSP_ROUTES("DSP2"),
 	MADERA_DSP_ROUTES("DSP3"),
 
-	{ "DSP2 Preloader", NULL, "DSP2 Virtual Input" },
+	{ "DSP2", NULL, "DSP2 Virtual Input" },
 	{ "DSP2 Virtual Input", "Shared Memory", "DSP3" },
-	{ "DSP3 Preloader", NULL, "DSP3 Virtual Input" },
+	{ "DSP3", NULL, "DSP3 Virtual Input" },
 	{ "DSP3 Virtual Input", "Shared Memory", "DSP2" },
 
 	{ "DSP1 Trigger Out", NULL, "SYSCLK" },
@@ -1501,15 +1501,15 @@ static const struct snd_soc_dapm_route cs47l35_dapm_routes[] = {
 	{ "DSP2 Trigger Output", "Switch", "DSP2" },
 	{ "DSP3 Trigger Output", "Switch", "DSP3" },
 
-	{ "DSP1 Preloader", NULL, "DSP Virtual Input" },
+	{ "DSP1", NULL, "DSP Virtual Input" },
 	{ "DSP1 Trigger Out", NULL, "DSP1 Virtual Output" },
 	{ "DSP1 Virtual Output", NULL, "SYSCLK" },
 
-	{ "DSP2 Preloader", NULL, "DSP Virtual Input" },
+	{ "DSP2", NULL, "DSP Virtual Input" },
 	{ "DSP2 Trigger Out", NULL, "DSP2 Virtual Output" },
 	{ "DSP2 Virtual Output", NULL, "SYSCLK" },
 
-	{ "DSP3 Preloader", NULL, "DSP Virtual Input" },
+	{ "DSP3", NULL, "DSP Virtual Input" },
 	{ "DSP3 Trigger Out", NULL, "DSP3 Virtual Output" },
 	{ "DSP3 Virtual Output", NULL, "SYSCLK" },
 
