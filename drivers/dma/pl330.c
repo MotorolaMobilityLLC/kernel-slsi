413c014d4dc0cc29f3fed32609e2910b8955c189
Jaejoon Yoo <joonyj7.yoo@samsung.com>
2017-01-02 21:06:20 +0900

    DMA: Modify register base address
    Change-Id: Ia9e0e042cbbaaf12f5e15b29f5506a15f57de736
    Signed-off-by: Jaejoon Yoo <joonyj7.yoo@samsung.com>

--- drivers/dma/pl330.c
+++ drivers/dma/pl330.c
@@ -2954,7 +2954,7 @@ int pl330_dma_debug(struct dma_chan *chan)
 		return -EINVAL;
 
 	thrd = pch->thread;
-	regs = &pch->dmac->base;
+	regs = pch->dmac->base;
 
 	idx = 1 - thrd->lstenq;
 	if (thrd->req[idx].desc != NULL) {
