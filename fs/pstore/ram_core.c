1fc884adeeb5a5ad5a33ac96016488df4bf90d85
Hosung Kim <hosung0.kim@samsung.com>
2017-04-26 14:09:24 +0900

    [COMMON] fs: pstore: add hooking platform functions with DSS
    Change-Id: Ib1b0d29001e647b86c644248eafa02e051d069da
    Signed-off-by: Hosung Kim <hosung0.kim@samsung.com>

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -276,6 +276,9 @@ static int notrace persistent_ram_update_user(struct persistent_ram_zone *prz,
 	struct persistent_ram_buffer *buffer = prz->buffer;
 	int ret = unlikely(__copy_from_user(buffer->data + start, s, count)) ?
 		-EFAULT : 0;
+
+	dbg_snapshot_hook_pmsg(buffer->data + start, count);
+
 	persistent_ram_update_ecc(prz, start, count);
 	return ret;
 }
