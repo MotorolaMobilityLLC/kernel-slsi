3f386f9333ffec9e0bf67874757c0a5475ef65c0
Eunseok Choi <es10.choi@samsung.com>
2017-05-04 17:50:03 +0900

    [COMMON] thermal: samsung: support ACPM TMU plugin
    add include/linux/sched/clock.h
    
    Change-Id: Ia4e795d45ca2aac5ddd9d59242764ba57f2021e4
    Signed-off-by: Eunseok Choi <es10.choi@samsung.com>

---
+++ drivers/thermal/samsung/exynos_acpm_tmu.c
@@ -0,0 +1,235 @@
+/*
+ * exynos_acpm_tmu.c - ACPM TMU plugin interface
+ *
+ * Copyright (C) 2017 Samsung Electronics
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/string.h>
+#include <linux/of_address.h>
+#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/debugfs.h>
+#include <linux/sched/clock.h>
+#include <soc/samsung/acpm_ipc_ctrl.h>
+#include "exynos_acpm_tmu.h"
+
+static bool cold_comp;
+static unsigned int acpm_tmu_ch_num, acpm_tmu_size;
+
+#define acpm_ipc_err_check() \
+	do { \
+		if (ret < 0) { \
+			pr_warn("[acpm_tmu] IPC error! type 0x%02x latency %llu ns ret %d\n", \
+					message.req.type, latency, ret); \
+			return -1; \
+		} \
+	} while (0)
+
+#define acpm_ipc_latency_check() \
+	do { \
+		if (latency > 2000000) { \
+			pr_info("[acpm_tmu] type 0x%02x latency %llu ns ret %d\n", \
+					message.req.type, latency, ret); \
+		} \
+	} while (0)
+
+/*
+ * TMU_IPC_INIT
+ */
+int exynos_acpm_tmu_set_init(struct acpm_tmu_cap *cap)
+{
+	struct ipc_config config;
+	union tmu_ipc_message message;
+	unsigned long long before, after, latency;
+	int ret;
+
+	memset(&message, 0, sizeof(message));
+
+	message.req.type = TMU_IPC_INIT;
+
+	config.cmd = message.data;
+	config.response = true;
+	config.indirection = false;
+
+	before = sched_clock();
+	ret = acpm_ipc_send_data(acpm_tmu_ch_num, &config);
+	after = sched_clock();
+	latency = after - before;
+
+	acpm_ipc_err_check();
+	acpm_ipc_latency_check();
+
+	memcpy(message.data, config.cmd, sizeof(message.data));
+
+	if (message.resp.ret & CAP_APM_IRQ)
+		cap->acpm_irq = true;
+
+	if (message.resp.ret & CAP_APM_DIVIDER)
+		cap->acpm_divider = true;
+
+	return 0;
+}
+
+/*
+ * TMU_IPC_READ_TEMP
+ *
+ * - tz: thermal zone index registered in device tree
+ */
+int exynos_acpm_tmu_set_read_temp(int tz, int *cur_temp)
+{
+	struct ipc_config config;
+	union tmu_ipc_message message;
+	unsigned long long before, after, latency;
+	int ret;
+
+	*cur_temp = 0;
+
+	memset(&message, 0, sizeof(message));
+
+	message.req.type = TMU_IPC_READ_TEMP;
+	message.req.tzid = tz;
+
+	config.cmd = message.data;
+	config.response = true;
+	config.indirection = false;
+
+	before = sched_clock();
+	ret = acpm_ipc_send_data(acpm_tmu_ch_num, &config);
+	after = sched_clock();
+	latency = after - before;
+
+	acpm_ipc_err_check();
+	acpm_ipc_latency_check();
+
+	memcpy(message.data, config.cmd, sizeof(message.data));
+
+	cold_comp = message.resp.cold;
+	*cur_temp = message.resp.temp;
+
+	return 0;
+}
+
+/*
+ * TMU_IPC_AP_SUSPEND
+ */
+int exynos_acpm_tmu_set_suspend(void)
+{
+	struct ipc_config config;
+	union tmu_ipc_message message;
+	unsigned long long before, after, latency;
+	int ret;
+
+	memset(&message, 0, sizeof(message));
+
+	message.req.type = TMU_IPC_AP_SUSPEND;
+
+	config.cmd = message.data;
+	config.response = true;
+	config.indirection = false;
+
+	before = sched_clock();
+	ret = acpm_ipc_send_data(acpm_tmu_ch_num, &config);
+	after = sched_clock();
+	latency = after - before;
+
+	acpm_ipc_err_check();
+	acpm_ipc_latency_check();
+
+	memcpy(message.data, config.cmd, sizeof(message.data));
+
+	cold_comp = message.resp.cold;
+
+	return 0;
+}
+
+/*
+ * TMU_IPC_CP_CALL
+ */
+int exynos_acpm_tmu_set_cp_call(void)
+{
+	struct ipc_config config;
+	union tmu_ipc_message message;
+	unsigned long long before, after, latency;
+	int ret;
+
+	memset(&message, 0, sizeof(message));
+
+	message.req.type = TMU_IPC_CP_CALL;
+
+	config.cmd = message.data;
+	config.response = true;
+	config.indirection = false;
+
+	before = sched_clock();
+	ret = acpm_ipc_send_data(acpm_tmu_ch_num, &config);
+	after = sched_clock();
+	latency = after - before;
+
+	acpm_ipc_err_check();
+	acpm_ipc_latency_check();
+
+	memcpy(&message.data, config.cmd, sizeof(message.data));
+
+	return 0;
+}
+
+/*
+ * TMU_IPC_AP_RESUME
+ */
+int exynos_acpm_tmu_set_resume(void)
+{
+	struct ipc_config config;
+	union tmu_ipc_message message;
+	unsigned long long before, after, latency;
+	int ret;
+
+	memset(&message, 0, sizeof(message));
+
+	message.req.type = TMU_IPC_AP_RESUME;
+
+	config.cmd = message.data;
+	config.response = true;
+	config.indirection = false;
+
+	before = sched_clock();
+	ret = acpm_ipc_send_data(acpm_tmu_ch_num, &config);
+	after = sched_clock();
+	latency = after - before;
+
+	acpm_ipc_err_check();
+	acpm_ipc_latency_check();
+
+	memcpy(&message.data, config.cmd, sizeof(message.data));
+
+	return 0;
+}
+
+static int __init exynos_acpm_tmu_init(void)
+{
+	struct device_node *np;
+
+	np = of_find_node_by_name(NULL, "acpm_tmu");
+	if (!np)
+		return -ENODEV;
+
+	return acpm_ipc_request_channel(np, NULL, &acpm_tmu_ch_num, &acpm_tmu_size);
+}
+fs_initcall(exynos_acpm_tmu_init);
