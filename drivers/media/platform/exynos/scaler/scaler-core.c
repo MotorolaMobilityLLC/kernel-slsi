978e05ed1ddc516f1c3eee661288cc4bd4ab78a5
Janghyuck Kim <janghyuck.kim@samsung.com>
2017-08-16 14:10:24 +0900

    [COMMON] media: scaler: add minor version information
    Change-Id: I3e20742826a69c72fe87bb92b3e02c1ca15a0b27
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

--- drivers/media/platform/exynos/scaler/scaler-core.c
+++ drivers/media/platform/exynos/scaler/scaler-core.c
@@ -330,7 +330,8 @@ static const struct sc_fmt sc_formats[] = {
 
 /* must specify in revers order of SCALER_VERSION(xyz) */
 static const u32 sc_version_table[][2] = {
-	{ 0x02000000, SCALER_VERSION(5, 0, 0) }, /* SC_POLY */
+	{ 0x02000100, SCALER_VERSION(5, 0, 0) }, /* SC_POLY */
+	{ 0x02000000, SCALER_VERSION(5, 0, 0) },
 	{ 0x80060007, SCALER_VERSION(4, 2, 0) }, /* SC_BI */
 	{ 0x0100000f, SCALER_VERSION(4, 0, 1) }, /* SC_POLY */
 	{ 0xA0000013, SCALER_VERSION(4, 0, 1) },
