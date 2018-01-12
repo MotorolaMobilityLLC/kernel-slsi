6160123617ff570a3661aec2bc66ef8ca8daffdf
hgchu <hg.chu@samsung.com>
2018-01-12 14:50:33 +0900

    scsi: ufs: set 4KB alignment for UFS FFU
    Change-Id: I940459a3f3c45ef446968a4e028ef000261cf91a
    Signed-off-by: hgchu <hg.chu@samsung.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4597,6 +4597,8 @@ static int ufshcd_slave_alloc(struct scsi_device *sdev)
 
 	blk_queue_softirq_done(sdev->request_queue, ufshcd_done);
 
+	blk_queue_update_dma_alignment(sdev->request_queue, PAGE_SIZE - 1);
+
 	return 0;
 }
 
