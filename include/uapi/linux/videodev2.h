70ce52e483c184faa079577cf32b17fe237b0a4b
Janghyuck Kim <janghyuck.kim@samsung.com>
2018-03-23 16:36:37 +0900

    [media] v4l: add 'unordered' flag to format description ioctl
    For explicit synchronization it important for userspace to know if the
    format being used by the driver can deliver the buffers back to
    userspace
    in the same order they were queued with QBUF.
    
    Ordered streams fits nicely in a pipeline with DRM for example, where
    ordered buffer are expected.
    
    v2	- Improve documentation (Hans)
    
    Change-Id: I5f0361407c7e50cf7eadb754bc977ed28de87527
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -709,6 +709,7 @@ struct v4l2_fmtdesc {
 
 #define V4L2_FMT_FLAG_COMPRESSED 0x0001
 #define V4L2_FMT_FLAG_EMULATED   0x0002
+#define V4L2_FMT_FLAG_UNORDERED  0x0004
 
 	/* Frame Size and frame rate enumeration */
 /*
