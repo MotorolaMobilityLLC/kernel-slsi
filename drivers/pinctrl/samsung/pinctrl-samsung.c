6f1e3121d61d8810baafed7751a7616dd6cbd3f6
Jaehyoung Choi <jkkkkk.choi@samsung.com>
2018-05-17 14:25:45 +0900

    [COMMON] pinctrl: samsung: Fixed pinctrl suspend func
    1. Removed duplicate condition processing part.
    2. Fix return value in suspend function.
    
    Change-Id: I57dbb2286cc3d51a22c36b5f800729935aa8c15b
    Signed-off-by: Jaehyoung Choi <jkkkkk.choi@samsung.com>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1242,7 +1242,7 @@ static void samsung_pinctrl_suspend_dev(
 	int ret;
 
 	if (!drvdata->suspend)
-		return -EINVAL;
+		return;
 
 	if (!IS_ERR(drvdata->pctl_dev->p)) {
 		/* This is ignore to disable mux configuration. */
@@ -1252,9 +1252,6 @@ static void samsung_pinctrl_suspend_dev(
 	ret = pinctrl_force_sleep(drvdata->pctl_dev);
 	if (ret)
 	        dev_err(drvdata->dev, "could not set sleep pinstate %d\n", ret);
-
-	if (!drvdata->suspend)
-		return;
 
 	for (i = 0; i < drvdata->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &drvdata->pin_banks[i];
