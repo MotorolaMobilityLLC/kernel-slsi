ec470a075449ea5eff5d154b89e5bc9ca0fc870d
Tarun Karela <t.karela@samsung.com>
2018-05-26 23:37:24 +0100

    [9610] wlbt: Copy scsc drivers from 4.9 kernel (Android P)
    Initial commit to add scsc drivers from 4.9 kernel Leman WW.
    
    Change-Id: Ia026bb559de6e61523a116bc6c7cbfbac56de8cd
    SCSC-Bug-Id: SSB-40175
    Signed-off-by: Tarun Karela <t.karela@samsung.com>

---
+++ include/scsc/scsc_release.h
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *
+ * Copyright (c) 2014 - 2018 Samsung Electronics Co., Ltd. All rights reserved
+ *
+ *****************************************************************************/
+
+#ifndef _SCSC_RELEASE_H
+#define _SCSC_RELEASE_H
+
+#define SCSC_RELEASE_PRODUCT 6
+#define SCSC_RELEASE_ITERATION 27
+#define SCSC_RELEASE_CANDIDATE 4
+
+
+#endif
+
+
