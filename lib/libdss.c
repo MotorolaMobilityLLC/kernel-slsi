8c02497ea07ed452d3b2ac5eda9dd31e92973326
Youngwan Kim <y103.kim@samsung.com>
2018-07-11 11:14:56 +0900

    [COMMON] lib: libdss: change dss-reader to libdss
    It's to fix build error in environment not installed qemu-aarch64.
    
    Change-Id: I6156bb191bfbfd80f29b46c713b13379a9e9b050
    Signed-off-by: Youngwan Kim <y103.kim@samsung.com>

--- lib/libdss.c
+++ lib/libdss.c
@@ -74,28 +74,29 @@ int main(int argc, char *argv[])
 		}
 	}
 #ifdef CONFIG_DEBUG_SNAPSHOT_BINDER
-	for (i = 0; i < DSS_API_MAX_NUM << 2; i++)
-	{
-		printf("log[%.9f] = { 'type' : 'binder', 'cpu' : %d, 'trace_type' : %d, 'transaction_id' : %d, 'from_pid' : %d,"\
-				" 'from_tid' : %d, 'to_pid' : %d, 'to_tid' : %d, 'from_pid_comm' : '%s', 'from_tid_comm' : '%s',"\
-				" 'to_pid_comm' : '%s', 'to_tid_comm' : '%s', 'to_node_id' : %d, 'reply' : %d, 'flags' : 0x%x, 'code' : 0x%x,"\
+	for (i = 0; i < DSS_API_MAX_NUM << 2; i++) {
+		printf("log[%.9f] = { 'type' : 'binder', 'cpu' : %d, 'trace_type' : %d, 'transaction_id' : %d,"
+				" 'from_pid' : %d, 'from_tid' : %d, 'to_pid' : %d, 'to_tid' : %d,"
+				" 'from_pid_comm' : '%s', 'from_tid_comm' : '%s',"
+				" 'to_pid_comm' : '%s', 'to_tid_comm' : '%s', 'to_node_id' : %d,"
+				" 'reply' : %d, 'flags' : 0x%x, 'code' : 0x%x,"
 				" 'return_error' : %d, 'return_error_param' : %d, 'return_error_line' : %d}\n",
 				p->binder[i].time/1.0e9,
-				p->binder[i].cpu,
-				p->binder[i].base.trace_type, p->binder[i].base.transaction_id,
-				p->binder[i].base.from_pid, p->binder[i].base.from_tid, p->binder[i].base.to_pid, p->binder[i].base.to_tid,
+				p->binder[i].cpu, p->binder[i].base.trace_type, p->binder[i].base.transaction_id,
+				p->binder[i].base.from_pid, p->binder[i].base.from_tid,
+				p->binder[i].base.to_pid, p->binder[i].base.to_tid,
 				p->binder[i].base.from_pid_comm, p->binder[i].base.from_tid_comm,
 				p->binder[i].base.to_pid_comm, p->binder[i].base.to_tid_comm,
 				p->binder[i].transaction.to_node_id, p->binder[i].transaction.reply,
 				p->binder[i].transaction.flags, p->binder[i].transaction.code,
-				p->binder[i].error.return_error, p->binder[i].error.return_error_param, p->binder[i].error.return_error_line);
+				p->binder[i].error.return_error, p->binder[i].error.return_error_param,
+				p->binder[i].error.return_error_line);
 		if (p->binder[i].time == 0)
 			break;
 	}
 #endif
 #ifdef CONFIG_DEBUG_SNAPSHOT_ACPM
-	for (i = 0; i < DSS_LOG_MAX_NUM; i++)
-	{
+	for (i = 0; i < DSS_LOG_MAX_NUM; i++) {
 		printf("log[%.9f] = {  'type' : 'acpmlog', 'log' : '%s', 'value' : %d }\n",
 				p->acpm[i].acpm_time/1.0e9,
 				p->acpm[i].log,
