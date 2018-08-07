24d9c5e772a835ecb6e5cac2b2f6156c92dc4b9d
Wooyeon Kim <wooy88.kim@samsung.com>
2018-08-07 17:54:31 +0900

    [COMMON] fimc-is2: add code related to initial AE for 2T7SX
     PR JIRA ID: CPR-134
    
    Change-Id: Id77783ad9d32d165f3b613bfa07fc8e6aab34d30
    Signed-off-by: Wooyeon Kim <wooy88.kim@samsung.com>

--- drivers/media/platform/exynos/fimc-is2/sensor/module_framework/cis/fimc-is-cis-2t7sx.c
+++ drivers/media/platform/exynos/fimc-is2/sensor/module_framework/cis/fimc-is-cis-2t7sx.c
@@ -1653,6 +1653,7 @@ static struct fimc_is_cis_ops cis_ops = {
 	.cis_compensate_gain_for_extremely_br = sensor_cis_compensate_gain_for_extremely_br,
 	.cis_wait_streamoff = sensor_cis_wait_streamoff,
 	.cis_wait_streamon = sensor_cis_wait_streamon,
+	.cis_set_initial_exposure = sensor_cis_set_initial_exposure,
 };
 
 static int cis_2t7sx_probe(struct i2c_client *client,
@@ -1775,6 +1776,9 @@ static int cis_2t7sx_probe(struct i2c_client *client,
 		sensor_2t7sx_pllinfos = sensor_2t7sx_pllinfos_A;
 		sensor_2t7sx_max_setfile_num = ARRAY_SIZE(sensor_2t7sx_setfiles_A);
 	}
+
+	cis->use_initial_ae = of_property_read_bool(dnode, "use_initial_ae");
+	probe_info("%s use initial_ae(%d)\n", __func__, cis->use_initial_ae);
 
 	v4l2_i2c_subdev_init(subdev_cis, client, &subdev_ops);
 	v4l2_set_subdevdata(subdev_cis, cis);
