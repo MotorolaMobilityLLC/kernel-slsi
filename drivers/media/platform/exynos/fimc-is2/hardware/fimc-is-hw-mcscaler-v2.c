bd896f481b5c721ac60de4adfcbaeee808a877ee
Wooyeon Kim <wooy88.kim@samsung.com>
2018-08-14 16:50:22 +0900

    [COMMON] fimc-is: fix a bug TDNR mode setting at each other instance
     - if device is used a multi-instance, TDNR mode should set 2DNR mode
       as other instance is operated
    
     PR JIRA ID: CPR-166
    
    Change-Id: I25254bcf4a2579cdbd4b16609a3257d42fa6392a
    Signed-off-by: Wooyeon Kim <wooy88.kim@samsung.com>

--- drivers/media/platform/exynos/fimc-is2/hardware/fimc-is-hw-mcscaler-v2.c
+++ drivers/media/platform/exynos/fimc-is2/hardware/fimc-is-hw-mcscaler-v2.c
@@ -857,6 +857,7 @@ config:
 	msdbg_hw(2, "shot: mcsc_out_st[0x%lx]\n", instance, hw_ip,
 		mcsc_out_st);
 
+	hw_mcsc->instance = instance;
 	clear_bit(MCSC_RST_CHK, &mcsc_out_st);
 	set_bit(HW_CONFIG, &hw_ip->state);
 
@@ -957,7 +958,6 @@ int fimc_is_hw_mcsc_update_param(struct fimc_is_hw_ip *hw_ip,
 		control_cmd = true;
 		msdbg_hw(2, "update_param: hw_ip->instance(%d), control_cmd(%d)\n",
 			instance, hw_ip, hw_mcsc->instance, control_cmd);
-		hw_mcsc->instance = instance;
 	}
 
 	if (control_cmd || (lindex & LOWBIT_OF(PARAM_MCS_INPUT))
