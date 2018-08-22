930b39ba38592ac883a79315d50c8d439d938c88
hyesoo.yu <hyesoo.yu@samsung.com>
2018-08-22 14:42:58 +0900

    dma-buf: fix uninitialized variable on error state
    Change-Id: Ifdb8a7e013575a0cc7885c0522d86b2dafb0f854
    Signed-off-by: hyesoo.yu <hyesoo.yu@samsung.com>

--- drivers/dma-buf/dma-buf-trace.c
+++ drivers/dma-buf/dma-buf-trace.c
@@ -295,7 +295,7 @@ int dmabuf_trace_alloc(struct dma_buf *dmabuf)
 
 	buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
 	if (!buffer)
-		goto err_task;
+		return ret;
 
 	INIT_LIST_HEAD(&buffer->ref_list);
 
