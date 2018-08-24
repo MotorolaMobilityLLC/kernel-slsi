5311563a9e55fff34c96c4e7ef06bdf97f70500e
Purnendu Kapadia <p.kapadia@samsung.com>
2018-08-24 11:15:08 +0100

    [9610] wlbt: no sable genetion on wifi disconnect
    We decided to disable wireless disconnect trigger for now as a quick
    workaround and not generate sable on this trigger. On rapid wifi on/off
    we need to serialize tar generation and limit number of tar files we keep
    which requires more thought and testing which will be done in a separate
    bug.
    
    Change-Id: Ia42e5b870404507c8544c17e600f14b4f3615b1f
    SCSC-Bug-Id: SSB-43178
    Signed-off-by: Purnendu Kapadia <p.kapadia@samsung.com>

--- drivers/net/wireless/scsc/mlme.c
+++ drivers/net/wireless/scsc/mlme.c
@@ -2306,7 +2306,10 @@ int slsi_mlme_disconnect(struct slsi_dev *sdev, struct net_device *dev, u8 *mac,
 	SLSI_NET_DBG1(dev, SLSI_MLME, "mlme_disconnect_req(vif:%u, bssid:%pM, reason:%d)\n", ndev_vif->ifnum, mac, reason_code);
 
 #ifdef CONFIG_SCSC_LOG_COLLECTION
-	scsc_log_collector_schedule_collection(SCSC_LOG_HOST_WLAN, SCSC_LOG_HOST_WLAN_REASON_DISCONNECT);
+	/* stop sable generation as a workaround for now on wifi disconnect.
+	 * TODO: serialize tar generation in wlbtd
+	 * scsc_log_collector_schedule_collection(SCSC_LOG_HOST_WLAN, SCSC_LOG_HOST_WLAN_REASON_DISCONNECT);
+	 */
 #else
 	mx140_log_dump();
 #endif
