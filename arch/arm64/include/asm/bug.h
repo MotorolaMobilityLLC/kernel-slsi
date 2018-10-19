c53c3a838ee45530d34271bf0b2cac644ee880cf
luodw1 <luodw1@motorola.com>
2018-10-19 14:32:03 +0800

    arch: add log output in BUG() define
    Change-Id: Ide19e8d074c51a46219689676bc5cb5c4aa37ae4
    Signed-off-by: luodw1 <luodw1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1258099
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- arch/arm64/include/asm/bug.h
+++ arch/arm64/include/asm/bug.h
@@ -25,9 +25,10 @@
 #define __BUG_FLAGS(flags)				\
 	asm volatile (__stringify(ASM_BUG_FLAGS(flags)));
 
-#define BUG() do {					\
-	__BUG_FLAGS(0);					\
-	unreachable();					\
+#define BUG() do {						\
+	printk("kernel BUG at %s:%d!\n", __FILE__, __LINE__);	\
+	__BUG_FLAGS(0);						\
+	unreachable();						\
 } while (0)
 
 #define __WARN_FLAGS(flags) __BUG_FLAGS(BUGFLAG_WARNING|(flags))
