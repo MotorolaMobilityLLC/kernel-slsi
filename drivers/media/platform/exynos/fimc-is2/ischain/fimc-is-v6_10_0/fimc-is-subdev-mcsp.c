50c95d6c5796778053f2b99279f642adc2cff365
Wooyeon Kim <wooy88.kim@samsung.com>
2018-10-25 19:46:18 +0900

    [9610] fimc-is2: support MCSC per-frame flip mode
     - set flip mode to shot_ext->mcsc_flip[MCSC_PORT_MAX]
    
     PR JIRA ID: CPR-353
    
    Change-Id: I58bc273e9113904c21b4849acba0e682aecf58b6
    Signed-off-by: Wooyeon Kim <wooy88.kim@samsung.com>

--- drivers/media/platform/exynos/fimc-is2/ischain/fimc-is-v6_10_0/fimc-is-subdev-mcsp.c
+++ drivers/media/platform/exynos/fimc-is2/ischain/fimc-is-v6_10_0/fimc-is-subdev-mcsp.c
@@ -307,7 +307,14 @@ static int fimc_is_ischain_mxp_start(struct fimc_is_device_ischain *device,
 					queue->framecfg.bytesperline[1]), 16);
 
 	mcs_output->yuv_range = crange;
-	mcs_output->flip = (u32)queue->framecfg.flip >> 1; /* Caution: change from bitwise to enum */
+
+	if (frame->shot_ext->mcsc_flip[index - PARAM_MCS_OUTPUT0] != mcs_output->flip) {
+		mdbg_pframe("flip is changed(%d->%d)\n",
+			device, subdev, frame,
+			mcs_output->flip,
+			frame->shot_ext->mcsc_flip[index - PARAM_MCS_OUTPUT0]);
+		mcs_output->flip = frame->shot_ext->mcsc_flip[index - PARAM_MCS_OUTPUT0];
+	}
 
 #ifdef ENABLE_HWFC
 	if (test_bit(FIMC_IS_ISCHAIN_REPROCESSING, &device->state))
@@ -427,6 +434,7 @@ static int fimc_is_ischain_mxp_tag(struct fimc_is_subdev *subdev,
 	u32 pixelformat = 0;
 	u32 *target_addr;
 	bool change_pixelformat = false;
+	bool change_flip = false;
 
 	device = (struct fimc_is_device_ischain *)device_data;
 
@@ -499,6 +507,9 @@ static int fimc_is_ischain_mxp_tag(struct fimc_is_subdev *subdev,
 			pixelformat = node->pixelformat;
 		}
 
+		if (ldr_frame->shot_ext->mcsc_flip[index - PARAM_MCS_OUTPUT0] != mcs_output->flip)
+			change_flip = true;
+
 		inparm.x = mcs_output->crop_offset_x;
 		inparm.y = mcs_output->crop_offset_y;
 		inparm.w = mcs_output->crop_width;
@@ -518,6 +529,7 @@ static int fimc_is_ischain_mxp_tag(struct fimc_is_subdev *subdev,
 		if (!COMPARE_CROP(incrop, &inparm) ||
 			!COMPARE_CROP(otcrop, &otparm) ||
 			change_pixelformat ||
+			change_flip ||
 			!test_bit(FIMC_IS_SUBDEV_RUN, &subdev->state) ||
 			test_bit(FIMC_IS_SUBDEV_FORCE_SET, &leader->state)) {
 			ret = fimc_is_ischain_mxp_start(device,
