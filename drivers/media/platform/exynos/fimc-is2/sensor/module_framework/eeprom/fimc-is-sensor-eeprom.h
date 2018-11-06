5622421bc8edc50df5d473dfa13d045afecc6e82
dan huh <dan.huh@samsung.com>
2018-11-06 15:06:40 +0900

    [COMMON] fimc-is2: Add a sensor EEPROM common file
    PR JIRA ID: CPR-451
    
    Change-Id: Ie13c04cb85dbe955e78c61c2b60e3794205f5c72
    Signed-off-by: dan huh <dan.huh@samsung.com>

---
+++ drivers/media/platform/exynos/fimc-is2/sensor/module_framework/eeprom/fimc-is-sensor-eeprom.h
@@ -0,0 +1,22 @@
+/*
+ * Samsung Exynos5 SoC series Sensor driver
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef FIMC_IS_EEPROM_H
+#define FIMC_IS_EEPROM_H
+
+int fimc_is_eeprom_file_write(const char *file_name, const void *data,
+		unsigned long size);
+int fimc_is_eeprom_file_read(const char *file_name, const void *data,
+		unsigned long size);
+int fimc_is_eeprom_module_read(struct i2c_client *client, char *file_name,
+		char *data, unsigned long size);
+int fimc_is_sensor_eeprom_check_crc(char *data, size_t size);
+
+#endif
+
