c1f747d5b6ce41ddd59da9c340b96a09bcc12321
Jaejoon Yoo <joonyj7.yoo@samsung.com>
2018-11-20 17:16:09 +0900

    [RAMEN9610-8778][COMMON] soc: samsung: delete soc configuration
    Change-Id: Icb4feedb7655eae31743a7cad8f3be6dd6fa6c1f
    Signed-off-by: Jaejoon Yoo <joonyj7.yoo@samsung.com>

--- include/dt-bindings/soc/samsung/exynos-bcm_dbg.h
+++ include/dt-bindings/soc/samsung/exynos-bcm_dbg.h
@@ -12,20 +12,9 @@
 #define _DT_BINDINGS_EXYNOS_BCM_DBG_H
 
 /* BCM Pre-defined event type */
-#ifdef CONFIG_SOC_EXYNOS9610
 #define NO_PRE_DEFINE_EVT		0
 #define PEAK_LATENCY_FMT_EVT		1
 #define PRE_DEFINE_EVT_MAX		2
-#else
-#define NO_PRE_DEFINE_EVT               0
-#define LATENCY_FMT_EVT                 1
-#define MO_FMT_EVT                      2
-#define BURST_LENGTH_FMT_EVT            3
-#define REQ_BLOCK_FMT_EVT               4
-#define DATA_BLOCK_FMT_EVT              5
-#define REQ_TYPE_FMT_EVT                6
-#define PRE_DEFINE_EVT_MAX              7
-#endif
 
 #define BCM_STOP			0
 #define BCM_RUN				1
