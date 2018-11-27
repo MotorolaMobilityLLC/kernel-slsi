3a77e1b0fedb0d18a3a36f080af636cb9ae8c49c
Jiyoung Jeong <ji_0.jeong@samsung.com>
2018-11-27 09:35:09 +0900

    [RAMEN9610-9418][RAMEN9610-8873][COMMON] drivers: modem_if: change return type for CFI
    static int vnet_xmit => static netdev_tx_t vnet_xmit
    
    Change-Id: Ib03e51dfd6906ca7fea714fdbd4e4bd199101d4b
    Signed-off-by: Jiyoung Jeong <ji_0.jeong@samsung.com>

--- drivers/misc/modem_if/modem_io_device.c
+++ drivers/misc/modem_if/modem_io_device.c
@@ -1464,7 +1464,7 @@ static int vnet_stop(struct net_device *ndev)
 	return 0;
 }
 
-static int vnet_xmit(struct sk_buff *skb, struct net_device *ndev)
+static netdev_tx_t vnet_xmit(struct sk_buff *skb, struct net_device *ndev)
 {
 	struct vnet *vnet = netdev_priv(ndev);
 	struct io_device *iod = vnet->iod;
