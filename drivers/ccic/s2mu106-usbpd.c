7ce6632e4fea92153ec7acc8188bb1ec3a397193
a17671 <a17671@motorola.com>
2018-11-29 10:16:57 +0800

    usbpd:Fix the usbpd interrupt storm issue
    Samsung usbpd interrupt is triggered by level
    Instead of edge, the irq service handle shall
    Disable the interrupt first and later enable
    It after it got processed.
    
    Change-Id: I36502860acded3ce81546fa0554a169ee81805b8
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1275108
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/ccic/s2mu106-usbpd.c
+++ drivers/ccic/s2mu106-usbpd.c
@@ -2435,6 +2435,7 @@ static irqreturn_t s2mu106_irq_thread(int irq, void *data)
 	int ret = 0;
 	unsigned attach_status = 0, rid_status = 0;
 
+	disable_irq_nosync(irq);
 	dev_info(dev, "%s\n", __func__);
 
 	mutex_lock(&pd_data->accept_mutex);
@@ -2526,7 +2527,7 @@ hard_reset:
 	mutex_unlock(&pdic_data->lpm_mutex);
 out:
 	mutex_unlock(&pdic_data->_mutex);
-
+	enable_irq(irq);
 	return IRQ_HANDLED;
 }
 
