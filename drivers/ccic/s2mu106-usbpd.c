4238f2c7b379bc536519ae998ce35ed470270503
Junhan Bae <junhan84.bae@samsung.com>
2018-12-11 14:22:17 +0900

    [9610] drivers : fix try snk problem
    There is possibility that error occured about try-snk
    I fix it to operate better
    
    Change-Id: If23bbef1a42d5e6d0a8f2d3b452943b0972e6799
    Signed-off-by: Junhan Bae <junhan84.bae@samsung.com>

--- drivers/ccic/s2mu106-usbpd.c
+++ drivers/ccic/s2mu106-usbpd.c
@@ -80,10 +80,8 @@ static void s2mu106_src(struct i2c_client *i2c);
 static void s2mu106_snk(struct i2c_client *i2c);
 static void s2mu106_assert_rd(void *_data);
 static void s2mu106_assert_rp(void *_data);
-#if defined(CONFIG_DUAL_ROLE_USB_INTF)
 static int s2mu106_set_attach(struct s2mu106_usbpd_data *pdic_data, u8 mode);
 static int s2mu106_set_detach(struct s2mu106_usbpd_data *pdic_data, u8 mode);
-#endif
 static void s2mu106_usbpd_check_rid(struct s2mu106_usbpd_data *pdic_data);
 static int s2mu106_usbpd_read_reg(struct i2c_client *i2c, u8 reg, u8 *dest);
 static int s2mu106_usbpd_write_reg(struct i2c_client *i2c, u8 reg, u8 value);
@@ -2111,7 +2109,6 @@ static void s2mu106_usbpd_detach_init(struct s2mu106_usbpd_data *pdic_data)
 	struct usbpd_data *pd_data = dev_get_drvdata(dev);
 	struct i2c_client *i2c = pdic_data->i2c;
 	int ret = 0;
-	u8 data = 0;
 
 	dev_info(dev, "%s\n", __func__);
 
@@ -2133,24 +2130,6 @@ static void s2mu106_usbpd_detach_init(struct s2mu106_usbpd_data *pdic_data)
 	usbpd_reinit(dev);
 	/* for ccic hw detect */
 	s2mu106_usbpd_write_reg(i2c, S2MU106_REG_MSG_SEND_CON, S2MU106_RESET_REG_00);
-#ifdef CONFIG_CCIC_TRY_SNK
-	s2mu106_usbpd_read_reg(i2c, S2MU106_REG_PLUG_CTRL_PORT, &data);
-	data &= ~(S2MU106_REG_PLUG_CTRL_MODE_MASK | S2MU106_REG_PLUG_CTRL_RP_SEL_MASK);
-	data |= S2MU106_REG_PLUG_CTRL_DRP | S2MU106_REG_PLUG_CTRL_RP180;
-	s2mu106_usbpd_write_reg(i2c, S2MU106_REG_PLUG_CTRL_PORT, data);
-
-	s2mu106_usbpd_read_reg(i2c, S2MU106_REG_PLUG_CTRL, &data);
-	data |= S2MU106_REG_PLUG_CTRL_UFP_ATTACH_OPT;
-	s2mu106_usbpd_write_reg(i2c, S2MU106_REG_PLUG_CTRL, data);
-
-	s2mu106_usbpd_read_reg(i2c, S2MU106_REG_PLUG_CTRL_SET_RD, &data);
-	data &= ~S2MU106_REG_USB31_EN;
-	s2mu106_usbpd_write_reg(i2c, S2MU106_REG_PLUG_CTRL_SET_RD, data);
-
-	s2mu106_usbpd_read_reg(i2c, S2MU106_REG_PLUG_CTRL_DET, &data);
-	data |= S2MU106_REG_DET_RD_OR_VBUS;
-	s2mu106_usbpd_write_reg(i2c, S2MU106_REG_PLUG_CTRL_DET, data);
-#endif
 	pdic_data->rid = REG_RID_MAX;
 	pdic_data->check_rid_wa = false;
 	pdic_data->is_factory_mode = false;
@@ -2171,6 +2150,7 @@ static void s2mu106_usbpd_detach_init(struct s2mu106_usbpd_data *pdic_data)
 static void s2mu106_usbpd_notify_detach(struct s2mu106_usbpd_data *pdic_data)
 {
 	struct device *dev = pdic_data->dev;
+
 #if defined(CONFIG_IFCONN_NOTIFIER)
 	/* MUIC */
 	ifconn_event_work(pdic_data, IFCONN_NOTIFY_MUIC, IFCONN_NOTIFY_ID_ATTACH,
@@ -2206,6 +2186,7 @@ static void s2mu106_usbpd_notify_detach(struct s2mu106_usbpd_data *pdic_data)
 #else
 	s2mu106_rprd_mode_change(pdic_data, TYPE_C_ATTACH_DRP);
 #endif
+
 	}
 #else
 	usbpd_manager_plug_detach(dev, 1);
