457e472d785fe7dcd88dae7ed96ccb6d4f900421
Wooyeon Kim <wooy88.kim@samsung.com>
2019-01-02 16:38:03 +0900

    [COMMON] fimc-is2: adjust dma split for modified interface
     - DMA2 split to max for support PAFSTAT
     - CSIS2 uses DMA CH3
    
     PR JIRA ID: CPR-536
    
    Change-Id: I84d0cb5cd5c480a04699e382a16f1404f25e2914
    Signed-off-by: Wooyeon Kim <wooy88.kim@samsung.com>

--- drivers/media/platform/exynos/fimc-is2/sensor/csi2/fimc-is-hw-csi-v5_1.c
+++ drivers/media/platform/exynos/fimc-is2/sensor/csi2/fimc-is-hw-csi-v5_1.c
@@ -740,13 +740,14 @@ int csi_hw_s_dma_common_dynamic(u32 __iomem *base_reg, size_t size, unsigned int
 	} else if (GET_DMA_CH(dma_ch, 0)) {
 		matrix_num = 0;
 		sram0_split = max;
+		sram1_split = max; /* Due to PAFSTAT stat DMA limitation, set DMA2 split to max */
 	} else if (GET_DMA_CH(dma_ch, 1)) {
 		dma_pri_0 = 2;
 		matrix_num = 2;
 		sram1_split = max;
 	} else if (GET_DMA_CH(dma_ch, 2)) {
-		matrix_num = 0;
-		sram1_split = max;
+		matrix_num = 2;
+		sram1_split = 4; /* Rear2 uses a DMA3 as image out */
 	} else if (GET_DMA_CH(dma_ch, 3)) {
 		dma_pri_1 = 2;
 		matrix_num = 17;
