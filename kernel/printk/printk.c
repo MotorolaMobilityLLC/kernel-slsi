417dc81d1a85673f14ad09487133b9bfc2dcd086
zhaoxp3 <zhaoxp3@motorola.com>
2019-01-11 16:08:36 +0800

    printk: LOG_LINE_MAX to 2048 - PREFIX_MAX
    To output command line completely in kernel log
    
    Change-Id: I5ff1c905cf42f2b2847fa5c1971c9185d6cadb1d
    Signed-off-by: zhaoxp3 <zhaoxp3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1292880
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -436,7 +436,7 @@ static u32 clear_idx;
 #else
 #define PREFIX_MAX		32
 #endif
-#define LOG_LINE_MAX		(1024 - PREFIX_MAX)
+#define LOG_LINE_MAX		(2048 - PREFIX_MAX)
 
 #define LOG_LEVEL(v)		((v) & 0x07)
 #define LOG_FACILITY(v)		((v) >> 3 & 0xff)
