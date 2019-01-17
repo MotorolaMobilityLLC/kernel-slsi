8fa6094eabc5a9dd26b8acac1f351fe0c183da74
a17671 <a17671@motorola.com>
2019-01-17 15:49:00 +0800

    Revert "usbpd:Fix the usbpd interrupt storm issue"
    This reverts commit 636afbc58ed019b48aab5637d06d1c005a6d780a.
    Samsung change the interrupt from level to falling edge
    Revert the moto change to fix the usb pd storm
    Mot-CRs-fixed:(CR)
    
    Change-Id: I7bd5bd2ba2d2bf1e14655d48a4c8d0deb40991f4
    Reviewed-on: https://gerrit.mot.com/1296008
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/ccic/s2mu106-usbpd.c
+++ drivers/ccic/s2mu106-usbpd.c
@@ -2427,7 +2427,6 @@ static irqreturn_t s2mu106_irq_thread(int irq, void *data)
 	int ret = 0;
 	unsigned attach_status = 0, rid_status = 0;
 
-	disable_irq_nosync(irq);
 	dev_info(dev, "%s\n", __func__);
 
 	mutex_lock(&pdic_data->_mutex);
@@ -2512,7 +2511,7 @@ hard_reset:
 	mutex_unlock(&pdic_data->lpm_mutex);
 out:
 	mutex_unlock(&pdic_data->_mutex);
-	enable_irq(irq);
+
 	return IRQ_HANDLED;
 }
 
