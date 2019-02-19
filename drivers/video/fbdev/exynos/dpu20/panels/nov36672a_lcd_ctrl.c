f571f637153dc53a6eed41726cd147482d1cc77a
xingbin <xingbin@huaqin.com>
2019-02-19 15:53:33 +0800

    [Kane]:[user]: modify  frame rate  to 55fps for hix83112a and nov36672a
    Change-Id: Ide2c3b2c12a1d7caf55f1003b6f807249023446b
    Signed-off-by: xingbin <xingbin@huaqin.com>
    Signed-off-by: tanhua1 <tanhua1@mt.com>
    Reviewed-on: https://gerrit.mot.com/1314613
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/video/fbdev/exynos/dpu20/panels/nov36672a_lcd_ctrl.c
+++ drivers/video/fbdev/exynos/dpu20/panels/nov36672a_lcd_ctrl.c
@@ -70,6 +70,128 @@ struct decon_lcd nov36672a_lcd_info = {
  */
 void nov36672a_lcd_init(int id, struct decon_lcd *lcd)
 {
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_3[0],
+				SEQ_CMD_3[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_3 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_4[0],
+				SEQ_CMD_4[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_4 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_5[0],
+				SEQ_CMD_5[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_5 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_6[0],
+				SEQ_CMD_6[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_6 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_7[0],
+				SEQ_CMD_7[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_7 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_8[0],
+				SEQ_CMD_8[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_8 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_9[0],
+				SEQ_CMD_9[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_9 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_10[0],
+				SEQ_CMD_10[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_10 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_11[0],
+				SEQ_CMD_11[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_11 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_12[0],
+				SEQ_CMD_12[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_12 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_13[0],
+				SEQ_CMD_13[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_13 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_14[0],
+				SEQ_CMD_14[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_14 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_15[0],
+				SEQ_CMD_15[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_15 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_16[0],
+				SEQ_CMD_16[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_16 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_17[0],
+				SEQ_CMD_17[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_17 command.\n");
+	mdelay(1);
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_18[0],
+				SEQ_CMD_18[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_18 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_19[0],
+				SEQ_CMD_19[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_19 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_20[0],
+				SEQ_CMD_20[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_20 command.\n");
+	mdelay(1);
+
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_21[0],
+				SEQ_CMD_21[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_21 command.\n");
+	mdelay(1);
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_22[0],
+				SEQ_CMD_22[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_22 command.\n");
+	mdelay(1);
+	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
+				SEQ_CMD_23[0],
+				SEQ_CMD_23[1]) < 0)
+		dsim_err("fail to send SEQ_CMD_23 command.\n");
+	mdelay(1);
 
 	if (dsim_wr_data(id, MIPI_DSI_DCS_SHORT_WRITE_PARAM,
 				SEQ_CMD_0[0],
