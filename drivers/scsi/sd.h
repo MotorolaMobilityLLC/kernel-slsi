3def0106c4c998a482e8803ae59f5ebc41225cb9
JaeHun Jung <jh0801.jung@samsung.com>
2019-03-18 15:01:12 +0900

    [RAMEN9610-20756][COMMON] scsi: sd: Change scsi timeout form 30s to 5s.
    UFS has very fast response device. So, We decrease timeout for recovery.
    
    Change-Id: I4c0a6375413839a8495045a771ab50ff23edc614
    Signed-off-by: JaeHun Jung <jh0801.jung@samsung.com>

--- drivers/scsi/sd.h
+++ drivers/scsi/sd.h
@@ -16,7 +16,7 @@
 /*
  * Time out in seconds for disks and Magneto-opticals (which are slower).
  */
-#define SD_TIMEOUT		(30 * HZ)
+#define SD_TIMEOUT		(5 * HZ)
 #define SD_MOD_TIMEOUT		(75 * HZ)
 /*
  * Flush timeout is a multiplier over the standard device timeout which is
