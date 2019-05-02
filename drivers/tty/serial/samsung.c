6192ff572bda36ee4d014da77b1909d5ccf0c052
Youngmin Nam <youngmin.nam@samsung.com>
2019-05-02 16:38:22 +0900

    tty: serial: samsung: fix conflict during updating 4.14.113
    Change-Id: If2084faf3153b6060f1579d077f13221b0b6f391
    Signed-off-by: Youngmin Nam <youngmin.nam@samsung.com>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1074,12 +1074,9 @@ static void s3c24xx_serial_set_termios(struct uart_port *port,
 	wr_regl(port, S3C2410_ULCON, ulcon);
 	wr_regl(port, S3C2410_UBRDIV, quot);
 
-<<<<<<< HEAD
 	if (ourport->info->has_divslot)
 		wr_regl(port, S3C2443_DIVSLOT, udivslot);
-=======
 	port->status &= ~UPSTAT_AUTOCTS;
->>>>>>> android-4.14-p
 
 	umcon = rd_regl(port, S3C2410_UMCON);
 	if (termios->c_cflag & CRTSCTS) {
