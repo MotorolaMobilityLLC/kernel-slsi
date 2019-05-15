2df7956ddf293ed94cc9beb4f24ba5b4dbf7c008
Ivan Priest <i.priest@samsung.com>
2019-05-15 12:19:39 +0100

    [NEUS7920-477] wlbt: annotate service driver API
    Annotate service driver API calls with service ID.
    Also BT remove callback exit point.
    
    Change-Id: Id945c9ca61732abd0c604a1ff2f402b242357ba0
    SCSC-Bug-Id: SSB-52765
    Signed-off-by: Ivan Priest <i.priest@samsung.com>

--- drivers/misc/samsung/scsc_bt/scsc_bt_module.c
+++ drivers/misc/samsung/scsc_bt/scsc_bt_module.c
@@ -1476,6 +1476,7 @@ static void slsi_bt_service_remove(struct scsc_mx_module_client *module_client,
 		/* Wait forever for recovery_release_complete, as it will
 		 * arrive even if autorecovery is disabled.
 		 */
+		SCSC_TAG_INFO(BT_COMMON, "wait for recovery_release_complete\n");
 		wait_for_completion(&bt_service.recovery_release_complete);
 		reinit_completion(&bt_service.recovery_release_complete);
 
@@ -1498,6 +1499,9 @@ static void slsi_bt_service_remove(struct scsc_mx_module_client *module_client,
 
 done:
 	mutex_unlock(&bt_start_mutex);
+
+	SCSC_TAG_INFO(BT_COMMON,
+	      "BT service remove complete (%s %p)\n", module_client->name, mx);
 }
 
 /* BT service driver registration interface */
@@ -1588,6 +1592,9 @@ static void slsi_ant_service_remove(struct scsc_mx_module_client *module_client,
 
 done:
 	mutex_unlock(&ant_start_mutex);
+
+	SCSC_TAG_INFO(BT_COMMON,
+		      "ANT service remove complete (%s %p)\n", module_client->name, mx);
 }
 #endif
 
