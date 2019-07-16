dd81f86711be1c8a54c175f2d163f5f3c9e90c3a
Ivan Priest <i.priest@samsung.com>
2019-07-16 13:54:13 +0100

    [RAMEN9610-19422][9630] wlbt: add WLBT drivers 10.1.1.0
    Add WLBT driver drop for Neus
    
    From tag scsc_release_10.1.1_mx450_p_ww
    
    Change-Id: Ic7fe507ff007a870431f4dcbb6c263e49c334d9c
    SCSC-Bug-Id: RELS-3034
    Signed-off-by: Ivan Priest <i.priest@samsung.com>
    Signed-off-by: Kim Gunho <gunho.kim@samsung.com>

---
+++ include/scsc/scsc_release.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ *
+ * Copyright (c) 2014 - 2019 Samsung Electronics Co., Ltd. All rights reserved
+ *
+ *****************************************************************************/
+
+#ifndef _SCSC_RELEASE_H
+#define _SCSC_RELEASE_H
+
+#define SCSC_RELEASE_SOLUTION "mx250"
+
+#define SCSC_RELEASE_PRODUCT 10
+#define SCSC_RELEASE_ITERATION 1
+#define SCSC_RELEASE_CANDIDATE 1
+
+#define SCSC_RELEASE_POINT 0
+
+#endif
+
+
