741a10ce123a1e4f364a66224d2f141e7aa993c0
Youngsoo <youngss.kim@samsung.com>
2019-08-22 17:25:47 +0900

    [RAMEN9610-19423] wlbt: increase filter id for adnormal multicast
    Change-Id: Ib4e001d96015de9a9cd016a379c699a99a229453
    Signed-off-by: Youngsoo <youngss.kim@samsung.com>

--- drivers/net/wireless/scsc/dev.h
+++ drivers/net/wireless/scsc/dev.h
@@ -471,8 +471,8 @@ enum slsi_filter_id {
 	SLSI_OPT_IN_TCP4_FILTER_ID,						/* 0x87 */
 	SLSI_OPT_IN_TCP6_FILTER_ID,						/* 0x88 */
 #endif
-	SLSI_OPT_OUT_ABNORMAL_MULTICAST_ID,					/* 0x88 */
-	SLSI_REGD_MC_FILTER_ID,							/* 0x89 */
+	SLSI_OPT_OUT_ABNORMAL_MULTICAST_ID,				/* 0x89 */
+	SLSI_REGD_MC_FILTER_ID,							/* 0x8A */
 };
 #else
 
@@ -492,8 +492,8 @@ enum slsi_filter_id {
 	SLSI_OPT_IN_TCP4_FILTER_ID,						/* 0x88 */
 	SLSI_OPT_IN_TCP6_FILTER_ID,						/* 0x89 */
 #endif
-	SLSI_OPT_OUT_ABNORMAL_MULTICAST_ID,					/* 0x89 */
-	SLSI_REGD_MC_FILTER_ID,							/* 0x8a */
+	SLSI_OPT_OUT_ABNORMAL_MULTICAST_ID,				/* 0x8a */
+	SLSI_REGD_MC_FILTER_ID,							/* 0x8b */
 };
 
 #endif
