de85896b4e6d4aec2848a9f1cabdc7bc72f9f57b
Jinsu Park <js00.park@samsung.com>
2019-10-14 15:53:02 +0900

    [RAMEN9610-20563][9610] : drivers : ccic : fix ccic rp threshold operating Source
    Change-Id: I57e76dc68a94f1f8085b71d783f2b8145f816c99
    Signed-off-by: Jinsu Park <js00.park@samsung.com>

--- drivers/ccic/s2mu106-usbpd.c
+++ drivers/ccic/s2mu106-usbpd.c
@@ -2110,7 +2110,7 @@ static int s2mu106_usbpd_check_abnormal_attach(struct s2mu106_usbpd_data *pdic_d
 	u8 data = 0;
 
 	s2mu106_usbpd_set_threshold(pdic_data, PLUG_CTRL_RP,
-										S2MU106_THRESHOLD_1371MV);
+										S2MU106_THRESHOLD_1285MV);
 	msleep(20);
 
 	s2mu106_usbpd_read_reg(i2c, S2MU106_REG_PLUG_MON2, &data);
